e2dc72340d95641be2243c9afee674ce10547f98
Saumya Bharti <bhartisaumya22@gmail.com>
2024-11-03 21:03:07 +0530

    implemented websocket

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,62 @@
-console.log("second")
+import express from 'express'
+import {WebSocket, WebSocketServer} from 'ws'
+
+import {IMessage, MessageHandler} from './messageHandler'
+
+const app = express()
+
+app.get('/foo', (req, res) => {
+    res.send('bar')
+})
+
+const server = app.listen(8080)
+
+const wss = new WebSocketServer({server})
+
+
+const messageHandler = new MessageHandler()
+
+
+wss.on('connection', (ws) => {
+
+    ws.on('open', () => {
+        console.log('Client connected...') 
+    })
+
+    ws.on('message', (data) => {
+        const message: IMessage = JSON.parse(data.toString())
+
+        switch(message.type){
+            case 'register':
+                messageHandler.register(ws, message)
+                break;
+
+            case 'privateMessage':
+                messageHandler.private(ws, message)
+                break;
+
+            case 'roomMessage':
+                messageHandler.group(ws, message)
+                break;
+
+
+            case 'broadcast':
+                messageHandler.broadcast(ws, message)
+                break;
+
+            default:
+                console.log(`Error in the message format: ${message}`)
+
+        }
+    })
+
+    ws.on('close', () => {
+        messageHandler.userDisconnected(ws)
+    })
+})
+
+
+
+
+
+
---
+++ src/messageHandler.ts
@@ -0,0 +1,109 @@
+import {WebSocket} from 'ws'
+
+
+export interface IMessage {
+    userId: string,
+    targetUserId?: string,
+    chatRoom?: string,
+    text?: string,
+    type: string
+}
+
+export class MessageHandler{
+    private clients: Map<string, WebSocket>;
+    private chatRoomClients: Map<string, Set<WebSocket>>
+
+    constructor(){
+        this.clients = new Map<string, WebSocket>
+        this.chatRoomClients = new Map<string, Set<WebSocket>>
+
+    }
+
+    // register when connecting to the ws
+
+    register(ws: WebSocket, message: IMessage){
+        const {userId} = message
+        this.clients.set(userId, ws)
+    }
+
+    // send private message to single user
+
+    private(ws: WebSocket, message: IMessage){
+        const {targetUserId, userId, text} = message
+
+        if(!targetUserId)
+            return
+
+        const targetClient = this.clients.get(targetUserId)
+
+        if(targetClient && targetClient.readyState === WebSocket.OPEN){
+            targetClient.send(JSON.stringify({
+                from: userId,
+                text
+            }))
+
+        }
+    }
+
+    // send message to user present in the group
+
+    group(ws: WebSocket, message: IMessage){
+        const {chatRoom, userId, text} = message
+
+        if(!chatRoom)
+            return;
+
+        const roomClients = this.chatRoomClients.get(chatRoom)
+
+        if(!roomClients){
+            this.chatRoomClients.set(chatRoom, new Set())
+            this.chatRoomClients.get(chatRoom)?.add(ws)
+        }
+        else{
+            roomClients.forEach((client: WebSocket) => {
+                if(client.readyState === WebSocket.OPEN){
+                    client.send(JSON.stringify({
+                        from: userId,
+                        text 
+                    }))
+                }
+            })
+        }
+    }
+
+    // send message to all the active user
+
+    broadcast(ws: WebSocket, message: IMessage){
+        const {userId, text} = message
+
+        this.clients.forEach((client: WebSocket) => {
+            if(client.readyState === WebSocket.OPEN){
+                client.send(JSON.stringify({
+                    from: userId,
+                    text
+                }))
+            }
+        })
+    }
+
+    // clear map when user disconnected
+
+    userDisconnected(ws: WebSocket){
+        // Remove the user from the clients
+        this.clients.forEach((client, userId) => {
+            if(client === ws){
+                this.clients.delete(userId)
+                console.log(`Client with usersID: ${userId} disconnected`)
+            }
+
+        })
+
+        // Remove the user from chatroooms
+        this.chatRoomClients.forEach((clients, roomId) => {
+            clients.delete(ws)
+
+            if(clients.size === 0)
+                this.chatRoomClients.delete(roomId)
+        })
+    }
+}
